61f9db563086c42f682f31df6ccf0b4c464ab176
http-snehal <snehalkushwah24@gmail.com>
2026-06-05 11:02:21 +0530

    Display Pokémon name and type upon fetching data

--- pokemonFinder/index.js
+++ pokemonFinder/index.js
@@ -13,6 +13,12 @@ async function fetchPokemon() {
   }
   console.log(data);
   const pokemonImg = document.getElementById('pokemon');
+  const name = document.createElement('div');
+  name.innerHTML = `Name: ${data.name}<br>Type: ${data.types[0].type.name}`;
+  document.getElementById('app').appendChild(name);
+  name.style.marginTop = '2px';
+  name.style.color = '#af2d2d';
+  name.style.fontSize = '1.2rem';
   pokemonImg.src = data.sprites.front_default;
   pokemonImg.alt = data.name;
   pokemonImg.style.display = 'block';
